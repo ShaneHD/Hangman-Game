573861d39a98b06af6450b517b37fb21c55cc929
Shane <Shane@Shane-PC.home>
2014-08-17 11:29:56 +0000

    bla

--- Hangman/src/Main.java
+++ Hangman/src/Main.java
@@ -1,13 +1,6 @@
-import java.io.File;
-
-import javax.swing.UIManager;
-
-import ga.shane.hangman.Board;
-import ga.shane.hangman.HangmanWindow;
 import ga.shane.hangman.RandomWordPicker;
-import ga.shane.utilities.FileUtils;
 import ga.shane.utilities.GuiUtils;
-
+import java.io.File;
 
 /** @author http://www.shane.ga */
 public class Main {
